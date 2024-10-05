1f137bffbe3a794dc8558fbd5c67c8411eb17243
bhnord <b.nordemann@aol.com>
2024-10-05 13:31:36 -0400

    added hook interaction to switch scene

--- scripts/hook.gd
+++ scripts/hook.gd
@@ -4,13 +4,17 @@ class_name Hook
 var TYPE = randi() % 10
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(TYPE)
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func interact():
+	print("interacted")
 	SceneManager.switch_to_scene(SceneManager.Scene.PULLING_ROD)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Fish:
+		body.add_hook(self)
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Fish:
+		body.remove_hook(self)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
+class_name Fish
 
 @export var move_speed: float=100
 
 var money = 0
 var upgrades = null
-
+var nearby_hooks: Array[Hook] = []
 
 
 const SPEED = 300.0
@@ -19,8 +20,18 @@ func _physics_process(delta: float) -> void:
 	velocity=input_direction*move_speed
 	move_and_slide()
 
-func spend(amt:int)->bool:
-	if amt > money:
-		return false
-	money-=amt
-	return true
+func _process(_delta):
+	if Input.is_action_just_pressed("bite_hook"):
+		print("action logged")
+		var hook = nearby_hooks.pop_front()
+		if hook:
+			print("hook found")
+			hook.interact()
+	
+	
+	
+func add_hook(hook:Hook):
+	nearby_hooks.push_front(hook)
+	
+func remove_hook(hook:Hook):
+	nearby_hooks.erase(hook)
