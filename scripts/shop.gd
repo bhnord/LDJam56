632065c7df6851f830e90b1a04571b06fe316444
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2024-10-04 22:51:32 -0400

    Add error label

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
 func _ready() -> void:
-	$Strength/StrengthCost.text = "Strength Cost: " + str(update_cost(GameManager.strength))
+	$Strength/StrengthCost.text = "Strength Cost: " + str(get_upgrade_cost(GameManager.strength))
+	$Money.text = str(GameManager.money)
 
 func _on_strength_pressed() -> void:
-	GameManager.subtract_money(10 * GameManager.strength)
-	GameManager.strength += 1
-	update_label()
+	if GameManager.purchase_item(get_upgrade_cost(GameManager.strength)):
+		GameManager.strength += 1
+		update_label()
+	else:
+		$ErrorLabel.text = "Not Enough Money"
 	
 func update_label() -> void:
 	$Money.text = str(GameManager.money)
-	$Strength/StrengthCost.text = "Strength Cost: " + str(update_cost(GameManager.strength))
+	$Strength/StrengthCost.text = "Strength Cost: " + str(get_upgrade_cost(GameManager.strength))
+	$ErrorLabel.text = ""
 
 func _on_third_more_sinister_thing_pressed() -> void:
 	pass # Replace with function body.
@@ -19,5 +23,5 @@ func _on_third_more_sinister_thing_pressed() -> void:
 func _on_agility_pressed() -> void:
 	pass # Replace with function body.
 
-func update_cost(level : int) -> int: 
+func get_upgrade_cost(level : int) -> int: 
 	return (level + 1) * 10
