51bb9c806d51407966c26e194c61586a8718a216
Matt <cataslay@gmail.com>
2024-10-05 17:40:02 -0400

    Finished creating the map

--- scenes/Map/player_map.gd
+++ scenes/Map/player_map.gd
@@ -1,25 +1,36 @@
 extends CharacterBody2D
 class_name MapFish
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var move_speed = 125.0
+
+var loc_close: MapLocation= null;
 
 
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		print("Accept Command")
+		if(loc_close != null):
+			print("loc is " + loc_close.name)
+			print("Entering Scene")
+			loc_close.interact()
+
+
+
+func add_hovering(loc: MapLocation):
+	remove_hovering(loc)
+	loc_close = loc
+	loc.handleEntered()
+
+func remove_hovering(loc: MapLocation):
+	if(loc == loc_close):
+		if(loc_close != null):
+			loc_close.handleExited()
+		loc_close = null
+	
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	var input_direction = Vector2(
+		Input.get_action_strength("right")-Input.get_action_strength("left"),
+		Input.get_action_strength("down")-Input.get_action_strength("up")
+	)
+	velocity=input_direction*move_speed
+	move_and_collide(velocity * delta)
--- scripts/MapScripts/map_location.gd
+++ scripts/MapScripts/map_location.gd
@@ -2,17 +2,16 @@ extends Area2D
 class_name MapLocation
 
 
-var loc_name: String = "Beach"
-var switchTo: SceneManager.Scene = SceneManager.Scene.BEACH
+@export var loc_name: String = "Beach"
+@export var switchTo: SceneManager.Scene = SceneManager.Scene.BEACH
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	%LocationName.text = loc_name
 
 func interact():
-	print("Location Node has been interacted with: Function node name is" + "")
+	print("Location Node has been interacted with: Function node name is" + loc_name)
 	SceneManager.switch_to_scene(switchTo)
 	
-
 
 
 func handleEntered() -> void:
@@ -23,11 +22,12 @@ func handleExited() -> void:
 	%LocationName.visible = false;
 
 func _on_body_entered(body: Node2D) -> void:
-		if body is MapFish:
-				body.add_hovering(self)
-				handleEntered()
+	print(body)
+	if body is MapFish:
+			body.add_hovering(self)
+			handleEntered()
 
 func _on_body_exited(body: Node2D) -> void:
-		if body is MapFish:
-				body.remove_hovering(self)
-				handleExited()
+	print(body)
+	if body is MapFish:
+			body.remove_hovering(self)
