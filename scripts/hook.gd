debd7c2db241b73d03cb44fd1240d118e5c55453
bhnord <b.nordemann@aol.com>
2024-10-05 12:26:45 -0400

    add hook class

---
+++ scripts/hook.gd
@@ -0,0 +1,17 @@
+extends Area2D
+class_name Hook
+
+var TYPE = randi() % 10
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(TYPE)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _interact():
+	pass
+	## start reeling sequence here
