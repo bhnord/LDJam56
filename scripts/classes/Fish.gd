309c4742582d85b81830d8b60d5af01319226182
bhnord <b.nordemann@aol.com>
2024-10-06 12:29:40 -0400

    added fishy movements

--- scripts/classes/Fish.gd
+++ scripts/classes/Fish.gd
@@ -13,6 +13,24 @@ func _physics_process(delta: float) -> void:
 		Input.get_action_strength("right")-Input.get_action_strength("left"),
 		Input.get_action_strength("down")-Input.get_action_strength("up")
 	)
+	
+	if Input.is_action_pressed("right"):
+		$FishSide.flip_h=false
+		$FishTop.visible=false
+		$FishSide.visible=true
+	elif Input.is_action_pressed("left"):
+		$FishSide.flip_h=true
+		$FishTop.visible=false
+		$FishSide.visible=true
+	elif Input.is_action_pressed("down"):
+		$FishTop.flip_v=true
+		$FishTop.visible=true
+		$FishSide.visible=false
+	else:
+		$FishTop.flip_v=false
+		$FishTop.visible=true
+		$FishSide.visible=false
+		
 	velocity=input_direction*move_speed
 	move_and_slide()
 
